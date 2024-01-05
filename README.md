6518e3e9da0b4c211be570174aa935a68dbf8365
Oli <oli@hoodieboi.dev>
2024-01-05 04:25:30 +0100

    Rewrite entire app
    Add configuration with environment variables
    Add optional repeating feature
    Add support for updating multiple records
    Add README.md
    Remove logging to file
    Improve async handling
    Improve log message clarity
    Improve error detection

---
+++ README.md
@@ -0,0 +1,37 @@
+# ddns-js
+
+Automatically set CloudFlare [DNS records](https://www.cloudflare.com/en-gb/learning/dns/dns-records/dns-a-record/) (only works with `A` records) to own public ip.
+
+## Environment variables
+
+### API_TOKEN
+
+> Required: yes
+
+The [Cloudflare API token](https://developers.cloudflare.com/fundamentals/api/get-started/create-token/) used for authorization.
+
+Example: `API_TOKEN=sWh9FHA5ow28zgdwTm8s4J0kUo8Td0imxhsNo9xJ`
+
+### RECORDS
+
+> Required: yes
+
+The [zone id](https://developers.cloudflare.com/api/operations/zones-get) and [record id](https://developers.cloudflare.com/api/operations/dns-records-for-a-zone-list-dns-records) of one or several DNS records. Zone id and record id are seperated by `/` and multiple records are seperated by `,`. Records are processed from left to right, waiting for the previous record to finish processing before continuing.
+
+#### Format
+
+`zone_id/record_id`  
+**OR**  
+`zone_id1/record_id1,zone_id2/record_id2,zone_id3/record_id3,...`
+
+#### Examples
+
+`RECORDS=cy94bddzspk61hg6foug0ne5149xx6ow/l9pjomh92a9bu0ek6v4579bu0h1cc6rx`  
+`RECORDS=cy94bddzspk61hg6foug0ne5149xx6ow/l9pjomh92a9bu0ek6v4579bu0h1cc6rx,cy94bddzspk61hg6foug0ne5149xx6ow/wl76os6x901dbg3533zwpqyaf31i56ay`  
+`RECORDS=cy94bddzspk61hg6foug0ne5149xx6ow/l9pjomh92a9bu0ek6v4579bu0h1cc6rx,fn2l48m2oj5t8u4vr1wznnrmu9ugc4eo/2va22fkitg20flj42ncl1txu43m1pc2l`  
+
+### REPEAT_MILLISECONDS
+
+> Required: no
+
+The delay in milliseconds (integer) to wait before repeating the check of all records. If omitted the program will process the records once and exit.
--- main.js
+++ main.js
@@ -1,93 +1,180 @@
-const fs = require('fs');
+require('dotenv/config')
 
-const LOG_FILE = '/var/log/ddns-js.log'
-const log = (content) => {
-    fs.writeFile(LOG_FILE, `${new Date().toLocaleString()}\n${content}\n`, () => {})
+const {API_TOKEN, RECORDS, REPEAT_MILLISECONDS} = process.env
+if (!API_TOKEN || !RECORDS) {
+    console.error('Missing required env variable')
+    process.exit()
 }
 
-try {
-    const axios = require('axios').default;
+function formatErrors(errors) {
+    return errors.map(e => `  Code ${e.code}: ${e.message}`).join('\n')
+}
 
-    const API_TOKEN = 'Ls2WZsuBmW6ea_rUHNZjh51xQBr1wckNxU57OTuG';
-    const ZONE_ID = 'a52fe7bbf543e784d74dd3a534fdfc11';
-    const RECORD_ID = '0b7badecf19bb0f66c88c1e05fa6dc07';
-
-    const sendGetRequest = async (url, config) => {
-        return await axios.get(url, config)
-            .then(response => {
-                return response;
-            })
-            .catch(reason => console.error(reason));
-    }
-
-    const sendPutRequest = async (url, data, config) => {
-        return await axios.put(url, data, config)
-            .then(response => {
-                return response;
-            })
-            .catch(reason => console.error(reason));
-    }
-
-    const getDnsRecord = async (zone_id, dns_record_id) => {
-        return await sendGetRequest(
-            `https://api.cloudflare.com/client/v4/zones/${zone_id}/dns_records/${dns_record_id}`,
-            {
-                'headers': {
-                    'Authorization': `Bearer ${API_TOKEN}`,
-                    'Content-Type': 'application/json'
-                }
+function getDnsRecord(zone_id, dns_record_id) {
+    return new Promise(resolve => {
+        const url = `https://api.cloudflare.com/client/v4/zones/${zone_id}/dns_records/${dns_record_id}`
+        fetch(url, {
+            headers: {
+                'Authorization': `Bearer ${API_TOKEN}`,
+                'Content-Type': 'application/json',
             }
-        );
-    }
-
-    const updateDnsRecord = async (zone_id, dns_record_id, data) => {
-        return await sendPutRequest(
-            `https://api.cloudflare.com/client/v4/zones/${zone_id}/dns_records/${dns_record_id}`,
-            data,
-            {
-                'headers': {
-                    'Authorization': `Bearer ${API_TOKEN}`,
-                    'Content-Type': 'application/json'
-                }
+        })
+        .then(response => response.json())
+        .then(json => {
+            if (!json.success) {
+                console.error(`Error occured while getting DNS record content:\n${formatErrors(json.errors)}\n`)
+                resolve(undefined)
+                return
             }
-        );
-    }
-
-    const getPublicIp = async () => {
-        return await sendGetRequest('http://icanhazip.com').then(response => {
-            return response.data.replace('\n', '');
-        });
-    }
-
-    const getRegisteredIp = async () => {
-        return await getDnsRecord(ZONE_ID, RECORD_ID).then(response => {
-            return response.data.result.content;
-        });
-    }
-
-    getRegisteredIp().then(registered_ip => {
-        getPublicIp().then(current_ip => {
-
-            var output = `Current IP:\t${current_ip}\nRegistered IP:\t${registered_ip}\n\n`
-
-            if (current_ip != registered_ip) {
-                output += 'IP does not match, updating CloudFlare...'
-
-                updateDnsRecord(ZONE_ID, RECORD_ID, {
-                    'name': '@',
-                    'type': 'A',
-                    'content': current_ip,
-                    'ttl': 1,
-                    'proxied': true
-                });
-
-            } else {
-                output += 'IP matches, doing nothing.'
-            }
-            log(output)
+            resolve(json.result)
         })
     })
+}
 
-} catch (e) {
-    log(e)
+function updateDnsRecord(zone_id, dns_record_id, data) {
+    const url = `https://api.cloudflare.com/client/v4/zones/${zone_id}/dns_records/${dns_record_id}`
+    return fetch(url, {
+        method: 'PUT',
+        headers: {
+            'Authorization': `Bearer ${API_TOKEN}`,
+            'Content-Type': 'application/json',
+        },
+        body: JSON.stringify(data),
+    })
+}
+
+function getPublicIp() {
+    return new Promise(resolve => {
+        fetch('http://icanhazip.com')
+        .then(response => response.text())
+        .then(text => {
+            const ip = text.replace('\n', '')
+            resolve(ip)
+        })
+    })
+}
+
+function checkDnsRecord(zone_id, dns_record_id, public_ip=null) {
+    return new Promise(resolve => {
+        console.log(`\nChecking DNS record ${zone_id}/${dns_record_id}`)
+    
+        const promises = [getDnsRecord(zone_id, dns_record_id)]
+        // Get public ip if none provided
+        if (public_ip) {
+            promises.push(public_ip)
+        } else {
+            promises.push(getPublicIp())
+        }
+    
+        Promise.all(promises)
+        .then(values => {
+            const [record, public_ip] = values
+    
+            if (!public_ip) {
+                console.error('Failed to get public IP')
+                resolve(undefined)
+                return
+            }
+            // Validate DNS record
+            if (!record) {
+                console.error('Failed to get DNS record')
+                resolve(undefined)
+                return
+            }
+            const record_type = record.type
+            if (record_type != 'A') {
+                console.error(`Record has unsupported type, expected A but got ${record_type}`)
+                resolve(undefined)
+                return
+            }
+            const record_ip = record.content
+        
+            // Compare IP addresses
+            console.log(`DNS record IP: ${record_ip}\nPublic IP:     ${public_ip}`)
+            if (record_ip != public_ip) {
+                console.log('IP does not match, updating DNS record...')
+                updateDnsRecord(zone_id, dns_record_id, {
+                    content: public_ip,
+                    name: record.name,
+                    proxied: record.proxied,
+                    type: record.type,
+                    comment: record.comment,
+                    tags: record.tags,
+                    ttl: record.ttl,
+                })
+                .then(response => response.json())
+                .then(json => {
+                    if (!json.success) {
+                        console.log(`Failed to update DNS record:\n${formatErrors(json.errors)}`)
+                        resolve(undefined)
+                        return
+                    }
+                    console.log(`DNS record has been updated, new IP: ${json.result.content}`)
+                    resolve(undefined)
+                })
+            } else {
+                console.log('IP matches, no action has been taken')
+                resolve(undefined)
+            }
+        })
+    })
+}
+
+function checkRecord(record, public_ip=null) {
+    return new Promise(resolve => {
+        const [zone_id, record_id] = record.split('/')
+        if (!zone_id?.match(/^[a-z0-9]{32}$/)) {
+            console.error(`Invalid zone id: ${zone_id}`)
+            resolve(undefined)
+            return
+        }
+        if (!record_id?.match(/^[a-z0-9]{32}$/)) {
+            console.error(`Invalid record id: ${record_id}`)
+            resolve(undefined)
+            return
+        }
+        checkDnsRecord(zone_id, record_id, public_ip)
+        .then(() => resolve(undefined))
+    })
+}
+
+function checkRecords(records) {
+    return new Promise(resolve => {
+        console.log('\nChecking records...')
+        if (records.includes(',')) {
+            records = records.split(',')
+            getPublicIp().then(public_ip => {
+                if (!public_ip) {
+                    console.error('Failed to get public IP')
+                    return
+                }
+                records.reverse()
+                function next() {
+                    if (records.length == 0) {
+                        resolve(undefined)
+                        return
+                    }
+                    const record = records.pop()
+                    checkRecord(record, public_ip)
+                    .then(next)
+                }
+                next()
+            })
+        } else {
+            checkRecord(records).then(() => {
+                resolve(undefined)
+            })
+        }
+    })
+}
+
+if (REPEAT_MILLISECONDS) {
+    const repeat_milliseconds = parseInt(REPEAT_MILLISECONDS)
+    function checkRecordsContinuous() {
+        checkRecords(RECORDS)
+        setTimeout(checkRecordsContinuous, repeat_milliseconds)
+    }
+    checkRecordsContinuous()
+} else {
+    checkRecords(RECORDS)
 }
