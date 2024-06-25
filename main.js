7fc40cc79ccb46366dd6b8863b136ea34bf1e536
Oli <oli@olillin.com>
2024-06-25 11:36:29 +0200

    Allow DNS record names as records in config

--- main.js
+++ main.js
@@ -9,38 +9,138 @@ if (!API_TOKEN) {
     process.exit()
 }
 
-// Read records from config.json
-let records = []
+;(async function main() {
+    // Read records from config.json
+    let records = []
 
-var schema
-try {
-    schema = JSON.parse(fs.readFileSync('config.schema.json').toString())
-} catch {
-    console.warn('Failed to read schema, JSON validation skipped')
+    var schema
+    try {
+        schema = JSON.parse(fs.readFileSync('config.schema.json').toString())
+    } catch {
+        console.warn('Failed to read schema, JSON validation skipped')
+    }
+    try {
+        const fileContent = fs.readFileSync('config.json')
+        const json = JSON.parse(fileContent.toString())
+        if (schema) {
+            const validator = new Validator()
+            const result = validator.validate(json, schema)
+            if (!result.valid) {
+                console.error('config.json is invalid, please make sure the file has the correct format as specified by config.schema.json\nErrors:\n' + result.errors.join('\n'))
+                process.exit()
+            }
+        }
+        records = await resolveRecords(json.records)
+    } catch (e) {
+        console.error('Failed to read records from config.json\nError:' + e)
+        process.exit()
+    }
+
+    if (REPEAT_MILLISECONDS) {
+        const repeat_milliseconds = parseInt(REPEAT_MILLISECONDS)
+        function checkRecordsContinuous() {
+            checkRecords(Array.from(records))
+            setTimeout(checkRecordsContinuous, repeat_milliseconds)
+        }
+        checkRecordsContinuous()
+    } else {
+        checkRecords(Array.from(records))
+    }
+})()
+
+async function resolveRecords(records) {
+    const result = []
+    await Promise.all(
+        records.map(async record => {
+            if (typeof record === 'string') {
+                const resolved = await resolveStringRecord(record)
+                if (!resolved) {
+                    console.error(`Unable to find record ${record}`)
+                    process.exit()
+                }
+                console.log(`Resolved name ${record} to ${JSON.stringify(resolved)}`)
+                result.push(resolved)
+            } else if (typeof record === 'object') {
+                result.push(record)
+            } else {
+                throw new Error(`Illegal record type ${typeof record}`)
+            }
+        })
+    )
+    return result
 }
-try {
-    const fileContent = fs.readFileSync('config.json')
-    const json = JSON.parse(fileContent.toString())
-    if (schema) {
-        const validator = new Validator()
-        const result = validator.validate(json, schema)
-        if (!result.valid) {
-            console.error('config.json is invalid, please make sure the file has the correct format as specified by config.schema.json\nErrors:\n' + result.errors.join('\n'))
-            process.exit()
+
+async function resolveStringRecord(record) {
+    const zones = await getZones()
+    const segments = record.split('.')
+    for (let i = -1; i > -segments.length; i--) {
+        const domain = segments.slice(i).join('.')
+        const zone = zones.find(zone => zone.name === domain)?.id
+        if (!zone) {
+            continue
+        }
+        const subdomain = segments.slice(undefined, i).join('.')
+        const zoneRecords = await getRecords(zone)
+        const zoneRecord = zoneRecords.find(zoneRecord => zoneRecord.name === record)?.id
+        if (!zoneRecord) {
+            continue
+        }
+        return {
+            zone_id: zone,
+            record_id: zoneRecord,
         }
     }
-    records = json.records
-} catch (e) {
-    console.error('Failed to read records from config.json\nError:' + e)
-    process.exit()
+    return undefined
 }
 
 function formatErrors(errors) {
     return errors.map(e => `  Code ${e.code}: ${e.message}`).join('\n')
 }
 
+function getZones() {
+    return new Promise((resolve, reject) => {
+        const url = 'https://api.cloudflare.com/client/v4/zones'
+        fetch(url, {
+            headers: {
+                Authorization: `Bearer ${API_TOKEN}`,
+                'Content-Type': 'application/json',
+            },
+        })
+            .then(response => response.json())
+            .then(json => {
+                if (!json.success) {
+                    console.error(`Error occured while getting zones:\n${formatErrors(json.errors)}\n`)
+                    reject()
+                    return
+                }
+                resolve(json.result)
+            })
+    })
+}
+
+function getRecords(zone_id) {
+    return new Promise((resolve, reject) => {
+        const url = `https://api.cloudflare.com/client/v4/zones/${zone_id}/dns_records`
+        fetch(url, {
+            headers: {
+                Authorization: `Bearer ${API_TOKEN}`,
+                'Content-Type': 'application/json',
+            },
+        })
+            .then(response => response.json())
+            .then(json => {
+                if (!json.success) {
+                    console.error(`Error occured while getting DNS records:\n${formatErrors(json.errors)}\n`)
+                    reject()
+                    return
+                }
+                resolve(json.result)
+            })
+    })
+}
+
 function getDnsRecord(zone_id, dns_record_id) {
-    return new Promise(resolve => {
+    return new Promise((resolve, reject) => {
         const url = `https://api.cloudflare.com/client/v4/zones/${zone_id}/dns_records/${dns_record_id}`
         fetch(url, {
             headers: {
@@ -52,7 +152,7 @@ function getDnsRecord(zone_id, dns_record_id) {
             .then(json => {
                 if (!json.success) {
                     console.error(`Error occured while getting DNS record content:\n${formatErrors(json.errors)}\n`)
-                    resolve(undefined)
+                    reject()
                     return
                 }
                 resolve(json.result)
@@ -83,7 +183,7 @@ function getPublicIp() {
 }
 
 function checkDnsRecord(zone_id, dns_record_id, public_ip = null) {
-    return new Promise(resolve => {
+    return new Promise((resolve, reject) => {
         console.log(`\nChecking DNS record ${zone_id}/${dns_record_id}`)
 
         const promises = [getDnsRecord(zone_id, dns_record_id)]
@@ -99,19 +199,19 @@ function checkDnsRecord(zone_id, dns_record_id, public_ip = null) {
 
             if (!public_ip) {
                 console.error('Failed to get public IP')
-                resolve(undefined)
+                reject()
                 return
             }
             // Validate DNS record
             if (!record) {
                 console.error('Failed to get DNS record')
-                resolve(undefined)
+                reject()
                 return
             }
             const record_type = record.type
             if (record_type != 'A') {
                 console.error(`Record has unsupported type, expected A but got ${record_type}`)
-                resolve(undefined)
+                reject()
                 return
             }
             const record_ip = record.content
@@ -148,16 +248,16 @@ function checkDnsRecord(zone_id, dns_record_id, public_ip = null) {
 }
 
 function checkRecord(record, public_ip = null) {
-    return new Promise(resolve => {
+    return new Promise((resolve, reject) => {
         const { zone_id, record_id } = record
         if (!zone_id?.match(/^[a-z0-9]{32}$/)) {
             console.error(`Invalid zone id: ${zone_id}`)
-            resolve(undefined)
+            reject()
             return
         }
         if (!record_id?.match(/^[a-z0-9]{32}$/)) {
             console.error(`Invalid record id: ${record_id}`)
-            resolve(undefined)
+            reject()
             return
         }
         checkDnsRecord(zone_id, record_id, public_ip).then(() => resolve(undefined))
@@ -184,15 +284,4 @@ function checkRecords(records) {
             next()
         })
     })
-}
-
-if (REPEAT_MILLISECONDS) {
-    const repeat_milliseconds = parseInt(REPEAT_MILLISECONDS)
-    function checkRecordsContinuous() {
-        checkRecords(Array.from(records))
-        setTimeout(checkRecordsContinuous, repeat_milliseconds)
-    }
-    checkRecordsContinuous()
-} else {
-    checkRecords(Array.from(records))
 }
